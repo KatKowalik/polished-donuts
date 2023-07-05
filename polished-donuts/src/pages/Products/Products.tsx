a5c839709084d3fb1f2124949ba8757f5e3d8b02
KatKowalik <katarzyna.kowalik2@gmail.com>
2023-07-05 15:44:25 -0400

    frontend connected with db

--- polished-donuts/src/pages/Products/Products.tsx
+++ polished-donuts/src/pages/Products/Products.tsx
@@ -9,9 +9,26 @@ import glutenIcon from "../../assets/icons/gluten-icon.svg";
 import milkIcon from "../../assets/icons/milk-icon.svg";
 import peanutIcon from "../../assets/icons/peanut-icon.svg";
 import donut from "../../assets/images/Pistachio-raspberry graphic.png";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 
 const Products = () => {
+    const [donuts, setDonuts] = useState([]);
+    const imgURL = "http://localhost:8080/"
+
+    useEffect(() => {
+        const getDonuts = async () => {
+            try{
+              const donutsData = await axios.get("http://localhost:8080/donuts")
+              setDonuts(donutsData.data)
+            } catch (error) {
+              console.log((error as Error).message)
+            }
+          }
+          getDonuts();
+    }, []);
+
     return (
         <section className="products">
             <Swiper 
@@ -27,14 +44,16 @@ const Products = () => {
                   slidesPerView={1}
                   modules={[Navigation, Pagination, Mousewheel, Keyboard]}
                   className="products__swiper">
+                { donuts.map(donut => {
+                    return (
                 <SwiperSlide>
-                    <article className="product-card green">
+                    <article className={`product-card ${donut.background}`}>
                         <div className="product-card__text">
                             <h2 className="product-card__title section-header">
-                                Pistachio Raspberry
+                                {donut.name}
                             </h2>
                             <p className="product-card__description paragraph">
-                                Experience a delightful fusion of flavors with our pistachio raspberry donut. Indulge in the rich, nutty essence of pistachio, perfectly complemented by the tangy sweetness of raspberry filling, all nestled within a fluffy, golden-brown donut.
+                                {donut.description}
                             </p>
                             <p className="product-card__contains label">Contains:</p>
                             <div className="product-card__icons">
@@ -43,9 +62,11 @@ const Products = () => {
                                 <img src={peanutIcon} alt="gluten icon" className="product-card__icon"/>
                             </div>
                         </div>
-                        <img src={donut} alt="donut graphic" className="product-card__product-img"/>
+                        <img src={`${imgURL}${donut.background}.png`} alt="donut graphic" className="product-card__product-img"/>
                     </article>
                 </SwiperSlide>
+                )
+                })}
             </Swiper>
         </section>
     )
