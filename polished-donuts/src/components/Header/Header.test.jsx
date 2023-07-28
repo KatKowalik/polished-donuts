733f14842a3ebb2dc58bd7879fdcccdf4835b718
KatKowalik <katarzyna.kowalik2@gmail.com>
2023-07-28 14:44:26 -0400

    added header's tests

--- polished-donuts/src/components/Header/Header.test.jsx
+++ polished-donuts/src/components/Header/Header.test.jsx
@@ -1,11 +1,38 @@
 import Header from "./Header";
 import { render, screen } from "@testing-library/react";
-import {it, expect} from "vitest";
+import {describe, it, expect} from "vitest";
 import { BrowserRouter } from "react-router-dom";
 
 
-it("renders logo", () => {
-    render(<Header />, {wrapper: BrowserRouter});
-    const logo = screen.queryByText(/Polished Donuts!/i);
-    expect(logo).toBeInTheDocument();
+describe("Header", ()=> {
+    it("renders logo", () => {
+        render(<Header />, {wrapper: BrowserRouter});
+        const logo = screen.queryByText(/Polished Donuts!/i);
+        expect(logo).toBeInTheDocument();
+    })
+
+    it("renders login button", () => {
+        render(<Header />, {wrapper: BrowserRouter});
+        expect(screen.getByText(/Log in!/i)).toBeInTheDocument();
+    })
+
+    it("renders singup button", () => {
+        render(<Header />, {wrapper: BrowserRouter});
+        expect(screen.getByText(/Sign up!/i)).toBeInTheDocument();
+    })
+
+    it("login button is not disabled", ()=> {
+        render(<Header/>, {wrapper: BrowserRouter});
+        const button = screen.getAllByRole("button");
+        expect(button[0]).not.toBeDisabled();
+      
+    })
+
+    it("signup button is not disabled", ()=> {
+        render(<Header/>, {wrapper: BrowserRouter});
+        const button = screen.getAllByRole("button");
+        expect(button[1]).not.toBeDisabled();
+    })
+
 })
+
