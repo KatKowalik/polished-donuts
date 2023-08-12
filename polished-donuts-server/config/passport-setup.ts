341c681a13a20da856fa32aea7c3f5d039e64b04
KatKowalik <katarzyna.kowalik2@gmail.com>
2023-08-12 14:48:10 -0400

    added passport setup

--- polished-donuts-server/config/passport-setup.ts
+++ polished-donuts-server/config/passport-setup.ts
@@ -1,13 +1,36 @@
 const passport = require("passport");
+const mongooseCtrl = require("mongoose");
 const JwtCookieComboStrategy = require('passport-jwt-cookiecombo');
 require("dotenv").config();
+const users = require("../schema/users");
+const bcrypt = require("bcryptjs")
 
+mongooseCtrl.connect(process.env.DB_STRING);
 const { SECRET_KEY } = process.env
 
-passport.use(new JwtCookieComboStrategy({
-    secretOrPublicKey: SECRET_KEY
-}, 
+const verifyCallback = (email: string, password: string, done: any) => {
+    users.findOne({ email: email })
+    .then((user: any) => {
+        if (!user) {
+            return done(null, false);
+        }
 
-(payload:any, done: Function) => {
-    return done(null, payload.user);
-})); 
+        const checkPassword = bcrypt.compareSync(password, user.password);
+        if (checkPassword) {
+            return done(null, user)
+        } else {
+            return done(null, false)
+        }
+    })
+    .catch((err: Error) => done(err))
+}
+
+const customFields = {
+    usernameField: "email",
+    passwordField: "password"
+}
+
+const strategy = new JwtCookieComboStrategy({secretOrPublicKey: SECRET_KEY}, verifyCallback, customFields, {session: false});
+
+
+passport.use(strategy);
